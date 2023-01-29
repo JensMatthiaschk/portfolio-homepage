d19cbe1f8731826f9d53e1fe4945ec436269539d
JensMatthiaschk <jens.matthiaschk@gmail.com>
2023-01-29 23:08:34 +0100

    add correct id definition to about section buttons

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -25,8 +25,8 @@ export default function About() {
                 <h1 className="font-['Montserrat'] md:text-8xl sm:text-5xl text-4xl font-extrabold w-auto" translate="no">Jens Matthiaschk</h1>
                 <h2 className="font-['Montserrat'] md:text-6xl sm:text-4xl text-2xl md:mt-5 mt-2">{t("about.profession")}</h2>
                 <p className="text-sm sm:text-base my-8 font-['Montserrat'] font-medium lg:w-[80%] w-[90%]">{t("about.description")}</p>
-                <div className="md:mt-10 sm:mt-8 mt-5 flex sm:flex-row flex-col gap-y-3">
-                    <Link to="Projects" spy={true} smooth={true} offset={0} duration={500}>
+                <div className="md:mt-10 sm:mt-8 mt-5 mb-20 flex sm:flex-row flex-col gap-y-3">
+                    <Link to={t('projects.section_title')} spy={true} smooth={true} offset={0} duration={500}>
                         <button className={currentTheme === 'dark' ? "badge p-4 sm:badge-lg badge-md sm:p-6 sm:text-2xl font-semibold badge-outline btn-dark hover:drop-shadow-[0_0_10px_var(--dm-third-color)] mr-3 transform-gpu" : "badge p-4 sm:badge-lg badge-md sm:p-6 sm:text-2xl font-semibold badge-outline btn-light hover:drop-shadow-[0_0_10px_var(--lm-third-color)] mr-3 transform-gpu"}>
                             <p className="mr-2">{t('about.button_projects')}</p>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
@@ -34,7 +34,7 @@ export default function About() {
                             </svg>
                         </button>
                     </Link>
-                    <Link to="Contact" spy={true} smooth={true} offset={0} duration={500}>
+                    <Link to={t('contact.section_title')} spy={true} smooth={true} offset={0} duration={500}>
                         <button className={currentTheme === 'dark' ? "badge p-4 sm:badge-lg badge-md sm:p-6 sm:text-2xl font-semibold badge-outline btn-dark hover:drop-shadow-[0_0_10px_var(--dm-third-color)] transform-gpu" : "badge p-4 sm:badge-lg badge-md sm:p-6 sm:text-2xl font-semibold badge-outline btn-light hover:drop-shadow-[0_0_10px_var(--lm-third-color)] transform-gpu"}>
                             <p className="mr-2">{t('about.button_contact')}</p>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
